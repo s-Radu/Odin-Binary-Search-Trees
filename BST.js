f7d62bd593febb023e417d442dcc27db50a63f8f
lil.void <tsraduu@gmail.com>
2024-05-31 21:12:40 +0100

    Tree class built, along with sorting the array and creating a balanced binary tree methods

--- BST.js
+++ BST.js
@@ -5,3 +5,29 @@ class Node {
 		this.right = null;
 	}
 }
+
+class Tree {
+	constructor(array) {
+		this.root = this.buildTree(array);
+	}
+
+	// Sort the array and eliminate the duplicates
+	buildTree(array) {
+		array = array.sort((a, b) => a - b);
+		array = array.filter((value, index, self) => self.indexOf(value) === index);
+
+		return this.buildSubTree(array, 0, array.length - 1);
+	}
+
+	// Turn the array into a balanced binary tree
+	buildSubTree(array, start, end) {
+		if (start > end) return;
+		let mid = Math.floor((start + end) / 2);
+		let node = new Node(mid);
+
+		node.left = this.buildTree(array, start, mid - 1);
+		node.right = this.buildTree(array, mid + 1, end);
+
+		return node;
+	}
+}
