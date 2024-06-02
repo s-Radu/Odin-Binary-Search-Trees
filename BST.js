ecd4ffa0b67c8c6359213dc7d041652a55657d51
lil.void <tsraduu@gmail.com>
2024-06-02 07:54:31 +0100

    made use of pretty print function that was given by the odin project.
    Small mods were needed in order for the function to work, and quite a few of my mistakes were corrected

--- BST.js
+++ BST.js
@@ -1,6 +1,6 @@
 class Node {
 	constructor(data) {
-		this.node = data;
+		this.data = data;
 		this.left = null;
 		this.right = null;
 	}
@@ -15,19 +15,36 @@ class Tree {
 	buildTree(array) {
 		array = array.sort((a, b) => a - b);
 		array = array.filter((value, index, self) => self.indexOf(value) === index);
-
 		return this.buildSubTree(array, 0, array.length - 1);
 	}
 
 	// Turn the array into a balanced binary tree
 	buildSubTree(array, start, end) {
-		if (start > end) return;
-		let mid = Math.floor((start + end) / 2);
-		let node = new Node(mid);
+		if (start > end) return null;
 
-		node.left = this.buildTree(array, start, mid - 1);
-		node.right = this.buildTree(array, mid + 1, end);
+		let mid = Math.floor((start + end) / 2);
+		let node = new Node(array[mid]);
+
+		node.left = this.buildSubTree(array, start, mid - 1);
+		node.right = this.buildSubTree(array, mid + 1, end);
 
 		return node;
 	}
 }
+
+const prettyPrint = (node, prefix = '', isLeft = true) => {
+	if (node === null) {
+		return;
+	}
+	if (node.right !== null) {
+		prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+	}
+	console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+	if (node.left !== null) {
+		prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+	}
+};
+
+// Test with an unsorted array
+let tree = new Tree([4, 2, 9, 6, 5, 3, 8, 1, 2, 12, 45, 33, 27, 7, 10, 7]);
+prettyPrint(tree.root);
